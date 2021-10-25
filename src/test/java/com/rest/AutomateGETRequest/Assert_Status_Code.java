cb2eced28711f01643c6a348812e6c17613f5785
Rashidul Hasan <rhratul12@gmail.com>
2021-10-26 04:16:00 +0600

    Status Code Assertion

---
+++ src/test/java/com/rest/AutomateGETRequest/Assert_Status_Code.java
@@ -0,0 +1,32 @@
+package com.rest.AutomateGETRequest;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class Assert_Status_Code {
+
+    @Test
+    public void assertStatusCode(){
+        // given() = we sent url, body,Authorization,Parameter or As like data by this Method.
+
+        // When() = what type of action we perform that sent by this method
+
+        // then() = Show /Validate  outcome by this method.
+        RestAssured.given()
+                            .baseUri("https://reqres.in/")
+                .when()
+                             .get("/api/users?page=2")
+                .then()
+                           .assertThat()
+                             .statusCode(200)
+                            .log().all();
+
+    }
+
+
+
+
+
+
+
+}
